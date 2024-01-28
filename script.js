99ddd4021f9d64841eebd18af491cc2167556f8e
Anastasia Garkushina <garkushina.nastya@gmail.com>
2024-01-28 03:01:01 +0300

    carousel buttons creation

--- script.js
+++ script.js
@@ -2,6 +2,34 @@ const carouselBreakpoint = 1028;
 const carouselItemsAmount = 5;
 let carouselExists = false;
 
+const btnBack = `<button class="btn btnBack">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" fill="none">
+    <circle cx="22" cy="22" r="22" transform="rotate(-180 22 22)" fill="currentColor"/>
+    <path
+        d="M24.5382 30.4614L16.0767 21.9999L24.5382 13.5384"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="square"
+        fill="none"
+    />
+    </svg>
+</button>`;
+const btnForward = `<button class="btn btnForward">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 44 44" fill="none">
+    <circle cx="22" cy="22" r="22" fill="currentColor"/>
+    <path
+        d="M19.4618 13.5384L27.9233 21.9999L19.4618 30.4615"
+        stroke="white"
+        stroke-width="2"
+        stroke-linecap="square"
+        fill="none"
+    />
+    </svg>
+</button>`;
+
+const vasykiCarousel = document.querySelector(".vasyki__carousel");
+const vasykiCarouselNav = `<div class="vasyki__carouselNav">${btnBack}${btnForward}</div>`;
+
 const createCarousel = (count) => {
   for (let i = 1; i <= count; i++) {
     //Create a new wrapper
@@ -19,6 +47,8 @@ const createCarousel = (count) => {
     //Append wrapper with found items
     wrapper.append(...items);
   }
+  //Add carousel navigation
+  vasykiCarousel.insertAdjacentHTML("beforeend", vasykiCarouselNav);
   carouselExists = true;
 };
 
@@ -27,6 +57,8 @@ const removeCarousel = () => {
   items.forEach((item) => {
     item.replaceWith(...item.childNodes);
   });
+  //Remove carousel navigation
+  vasykiCarousel.removeChild(vasykiCarousel.lastChild);
   carouselExists = false;
 };
 
